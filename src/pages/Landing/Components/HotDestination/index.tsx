aca86e33305722b46fc9eb95fbaf06c52d14bfd9
stanluu <42020235+stanluu@users.noreply.github.com>
2023-01-23 22:25:00 +0800

    Landing city choice part
    Landing city choice part

---
+++ src/pages/Landing/Components/HotDestination/index.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+import "./index.scss";
+
+
+
+function App() {
+
+    const cityList = [{
+        img: "https://tuassets.com/wp-content/reactpress/imgs/landing/SEATTLE.png?_t=1674213332",
+        name: "Seattle"
+    },{
+        img: "https://tuassets.com/wp-content/reactpress/imgs/landing/BOSTON.png?_t=1674213332",
+        name: "Boston"
+    },{
+        img: "https://tuassets.com/wp-content/reactpress/imgs/landing/MIAMI.png?_t=1674213332",
+        name: "Miami"
+    },{
+        img: "https://tuassets.com/wp-content/reactpress/imgs/landing/nullCity.svg",
+        name: ""
+    }]
+
+  return (
+    <div className="landing-HotDestination">
+        <div className="landing-HotDestination-title">
+            With us, enjoy your vacation at hot travel destinations at
+            <p>your own homes</p>
+        </div>
+        <div className="landing-HotDestination-city">
+            {cityList.map((item,key)=> {
+
+                    return (
+                        <div className="landing-HotDestination-city-each">
+                            <img src={item.img}/>
+
+                            <p>{item.name}</p>
+                        </div>
+                    )
+                }
+            )}
+        </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Landing/Components/TopProperties/index.tsx
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+
+
+import "./index.scss";
+
+function App() {
+
+    const dataList = [{
+        cityName: "Miami",
+        houses: [{
+            image:"https://tuassets.com/wp-content/reactpress/imgs/landing/house1.png?_t=1674482428",
+            price:"20,000",
+            city:"Malibu",
+            state:"CA",
+            bedroom:3,
+            bathroom:3
+        },{
+            image:"https://tuassets.com/wp-content/reactpress/imgs/landing/house2.png?_t=1674482428",
+            price:"8,000",
+            city:"Malibu",
+            state:"CA",
+            bedroom:3,
+            bathroom:3
+        },{
+            image:"https://tuassets.com/wp-content/reactpress/imgs/landing/house2.png?_t=1674482428",
+            price:"8,000",
+            city:"Malibu",
+            state:"CA",
+            bedroom:3,
+            bathroom:3
+        }]
+    },{
+        cityName: "Boston",
+        houses: [{
+            image:"https://tuassets.com/wp-content/reactpress/imgs/landing/house1.png?_t=1674482428",
+            price:"30,000",
+            city:"Malibu",
+            state:"CA",
+            bedroom:3,
+            bathroom:3
+        },{
+            image:"https://tuassets.com/wp-content/reactpress/imgs/landing/house2.png?_t=1674482428",
+            price:"9,000",
+            city:"Malibu",
+            state:"CA",
+            bedroom:3,
+            bathroom:3
+        },{
+            image:"https://tuassets.com/wp-content/reactpress/imgs/landing/house2.png?_t=1674482428",
+            price:"9,000",
+            city:"Malibu",
+            state:"CA",
+            bedroom:3,
+            bathroom:3
+        }]
+    },{
+        cityName: "Seattle",
+        houses: [{
+            image:"https://tuassets.com/wp-content/reactpress/imgs/landing/house1.png?_t=1674482428",
+            price:"40,000",
+            city:"Malibu",
+            state:"CA",
+            bedroom:3,
+            bathroom:3
+        },{
+            image:"https://tuassets.com/wp-content/reactpress/imgs/landing/house2.png?_t=1674482428",
+            price:"7,000",
+            city:"Malibu",
+            state:"CA",
+            bedroom:3,
+            bathroom:3
+        },{
+            image:"https://tuassets.com/wp-content/reactpress/imgs/landing/house2.png?_t=1674482428",
+            price:"7,000",
+            city:"Malibu",
+            state:"CA",
+            bedroom:3,
+            bathroom:3
+        }]
+    }]
+
+    const [curHouse, setCurHouse] = useState(dataList[0]);
+
+  return (
+    <div className="landing-TopProperties">
+        <div className="landing-TopProperties-title">
+            Top travel destinations
+            <p>Top properties</p>
+        </div>
+        <div className="landing-TopProperties-Cities">
+            {dataList.map((item, key)=>{
+                return(
+                    <button id={"landing-TopProperties-Cities-button"+key} className="landing-TopProperties-Cities-button" onClick={()=>setCurHouse(item)}>
+                        {item.cityName}
+                        <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g opacity="0.6">
+                                <path d="M23.789 23.7882H14.738M23.789 23.7882V14.7373M23.789 23.7882L9.64683 9.64609" stroke="black"/>
+                            </g>
+                        </svg>
+                    </button>
+                )
+            })}
+        </div>
+        <div className="landing-TopProperties-Houses">
+            {
+                curHouse.houses.map((item, key)=>{
+                    return (
+                        <div className="landing-TopProperties-Houses-house">
+                            <img src={item.image}/>
+                            <div className="houseInfoRow1">
+                                <div>
+                                    {"$"+item.price}
+                                    <span>/share</span>
+                                </div>
+                                <img src="https://tuassets.com/wp-content/reactpress/imgs/landing/like.svg"/>
+
+                            </div>
+                            <div className="houseInfoRow2">
+                                <p>{item.city+", "+item.state}</p>
+
+                                <strong>{item.bedroom+"BD, "+item.bathroom+"BA"}</strong>
+                            </div>
+
+                        </div>
+                    )
+                })
+            }
+        </div>
+
+
+    </div>
+  );
+}
+
+export default App;
